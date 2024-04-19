a5cdc0a96111ced3420775ffd3bd8fcf332e1268
x19441824 <x19441824@student.ncirl.ie>
2024-04-19 14:34:27 +0100

    Back to jest, save me :(

--- test/server.test.js
+++ test/server.test.js
@@ -2,76 +2,66 @@ const request = require('supertest');
 const app = require('../server.js');
 
 describe('Server API Tests', () => {
-  let chai, expect;
-
-  before(async () => {
-    const chaiModule = await import('chai');
-    chai = chaiModule.default;
-    expect = chaiModule.expect;
-  });
-
   let server;
 
-
-  before((done) => {
+  beforeAll((done) => {
     server = app.listen(4000, () => {
       console.log('Server started for testing.');
       done();
     });
   });
 
-  after((done) => {
-    server.close(() => {
-      console.log('Server closed for testing.');
+  afterAll((done) => {
+    if (server) {
+      server.close(() => {
+        console.log('Server closed for testing.');
+        done();
+      });
+    } else {
+      console.log('No server to close.');
       done();
-    });
+    }
   });
 
-  describe('GET /recipes', () => {
-    it('should fetch all recipes', (done) => {
-      request(app)
-        .get('/recipes')
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body.data).to.be.an('array');
-          done();
-        });
-    });
+  test('GET /recipes should fetch all recipes', (done) => {
+    request(app)
+      .get('/recipes')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data).toBeInstanceOf(Array);
+        done();
+      });
   });
 
-  describe('GET /search', () => {
-    it('should search recipes by a term', (done) => {
-      const searchTerm = 'chocolate';
-      request(app)
-        .get(`/search?query=${searchTerm}`)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          done();
-        });
-    });
+  test('GET /search should search recipes by a term', (done) => {
+    const searchTerm = 'chocolate';
+    request(app)
+      .get(`/search?query=${searchTerm}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
   });
 
-  describe('POST /recipes', () => {
-    it('should add a new recipe', (done) => {
-      const newRecipe = {
-        title: 'New Recipe',
-        description: 'Description of the new recipe',
-        ingredients: 'Ingredients of the new recipe',
-        preparation_time: 30,
-        difficulty: 'easy'
-      };
+  test('POST /recipes should add a new recipe', (done) => {
+    const newRecipe = {
+      title: 'New Recipe',
+      description: 'Description of the new recipe',
+      ingredients: 'Ingredients of the new recipe',
+      preparation_time: 30,
+      difficulty: 'easy'
+    };
 
-      request(app)
-        .post('/recipes')
-        .send(newRecipe)
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.body.message).to.equal('success');
-          done();
-        });
-    });
+    request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toBe('success');
+        done();
+      });
   });
 });
